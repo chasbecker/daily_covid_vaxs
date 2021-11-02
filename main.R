15d15b6144212fe3205d0e58be6c22d09893d7c1
Charles Becker <c.l.becker@outlook.com>
2021-11-01 17:24:24 -0700

    a commit

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 rm(list=ls())
 
-# this takes a while to read a few million rows
+# this takes a while to read a bunch of data
 daily <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 str(daily)
 
@@ -10,7 +10,8 @@ usa_daily <- daily %>%
              select( date, new_vaccinations_smoothed_per_million )
 
 
+plot_daily <- ggplot(data = usa_daily, mapping = aes( x = date, y = new_vaccinations_smoothed_per_million )) +
+              geom_point()
+#show the plot
 plot_daily
 
-
-plot(as.Date(usa_daily$date), usa_daily$new_vaccinations_smoothed_per_million)
